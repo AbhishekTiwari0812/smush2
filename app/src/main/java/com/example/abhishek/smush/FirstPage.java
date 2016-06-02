b4b8a3b2689b95df53c56b0ca63e906e89b40edc
Avijit Sinha <avijit2671994@gmail.com>
2016-06-03 00:51:34 +0530

    Made appropriate changes for SongPlayerService

--- app/src/main/java/com/example/abhishek/smush/FirstPage.java
+++ app/src/main/java/com/example/abhishek/smush/FirstPage.java
@@ -4,6 +4,7 @@ package com.example.abhishek.smush;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -24,33 +25,47 @@ public class FirstPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         button_start_player = (Button) findViewById(R.id.bt_start_playing);
         button_open_song_list = (Button) findViewById(R.id.bt_open_song_list);
         start_playing = true;
+
+        //TODO: FETCH THE LIST OF SONGS.
+        //TODO: SUPPLY IT TO THE SERVICE.
+
         //opens the music player app
         button_start_player.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (start_playing) {
-                    Intent intent = new Intent(getApplicationContext(), SongPage.class);
-                    startActivity(intent);
+                    Intent intent = new Intent(getApplicationContext(), SongPlayerService.class);
+                    startService(intent);
                     button_start_player.setText("Stop Playing");
                     start_playing = false;
                 } else {
-                    //TODO: release all resources and close the app.
+                    Intent intent = new Intent(getApplicationContext(), SongPlayerService.class);
+                    stopService(intent);
+                    start_playing = true;
+                    button_start_player.setText("Start Playing Again");
                 }
             }
         });
 
         //opens the list of all songs
+
         button_open_song_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SongList.class);
+                Intent intent = new Intent(getApplicationContext(), SongListPage.class);
                 startActivity(intent);
             }
         });
 
 
+    }
+
+    static void _(String str) {
+
+        Log.d("ERRRRROOOORRR::", "" + str);
     }
 }
--- app/src/main/java/com/example/abhishek/smush/TradeSecret.java
+++ app/src/main/java/com/example/abhishek/smush/TradeSecret.java
@@ -15,7 +15,7 @@ public class TradeSecret {
     public static void update_priority(Long song_id,Double time_played) {
         Long timestamp = System.currentTimeMillis()/1000L;
         Integer current_time_slot = (int)((timestamp%604800L)/14400L);
-        Song song = FirstPage.SONGS_IN_PHONE.get(song_id);
+        Song song = SongPlayerService.SONGS_IN_PHONE.get(song_id);
         Integer current_priority = get_or_update_priority(song_id,current_time_slot);
         Integer new_priority = current_priority + (int)(time_played-0.5)*100;
         if (new_priority < 1) {
@@ -25,7 +25,7 @@ public class TradeSecret {
             new_priority = 10000;
         }
         song.trade_secret[current_time_slot] = new_priority;
-        SharedPreferences pref_file = SongPage.context.getSharedPreferences(TRADE_SECRET_PREF_FILE, Context.MODE_PRIVATE);
+        SharedPreferences pref_file = SongPlayerService.CONTEXT.getSharedPreferences(TRADE_SECRET_PREF_FILE, Context.MODE_PRIVATE);
         SharedPreferences.Editor pref_file_editor = pref_file.edit();
         pref_file_editor.putInt(song_id.toString()+" "+current_time_slot.toString(),new_priority);
         pref_file_editor.commit();
@@ -34,7 +34,7 @@ public class TradeSecret {
     public static long get_next_song_id() {
         Long timestamp = System.currentTimeMillis()/1000L;
         Integer current_time_slot = (int)((timestamp%604800L)/14400L);
-        Set<Long> song_key_set = FirstPage.SONGS_IN_PHONE.keySet();
+        Set<Long> song_key_set = SongPlayerService.SONGS_IN_PHONE.keySet();
         while(song_key_set.isEmpty()) {
             try {
                 Thread.sleep(1000L);
@@ -42,19 +42,19 @@ public class TradeSecret {
                 e.printStackTrace();
             }
 
-            song_key_set = FirstPage.SONGS_IN_PHONE.keySet();
+            song_key_set = SongPlayerService.SONGS_IN_PHONE.keySet();
         }
 
         Long total_priority = 0L;
         for (Long song_key:
              song_key_set) {
-            total_priority += FirstPage.SONGS_IN_PHONE.get(song_key).trade_secret[current_time_slot];
+            total_priority += SongPlayerService.SONGS_IN_PHONE.get(song_key).trade_secret[current_time_slot];
         }
 
         Long random_selector = (long)(Math.random() * total_priority + 1);
         for (Long song_key :
                 song_key_set) {
-            random_selector -= FirstPage.SONGS_IN_PHONE.get(song_key).trade_secret[current_time_slot];
+            random_selector -= SongPlayerService.SONGS_IN_PHONE.get(song_key).trade_secret[current_time_slot];
             if (random_selector < 0) {
                 return song_key;
             }
@@ -64,9 +64,9 @@ public class TradeSecret {
     }
 
     private static int get_or_update_priority(Long song_id, Integer current_time_slot) {
-        if (FirstPage.SONGS_IN_PHONE.get(song_id).trade_secret == null) {
-            FirstPage.SONGS_IN_PHONE.get(song_id).trade_secret = new int[42];
-            SharedPreferences pref_file = SongPage.context.getSharedPreferences(TRADE_SECRET_PREF_FILE, Context.MODE_PRIVATE);
+        if (SongPlayerService.SONGS_IN_PHONE.get(song_id).trade_secret == null) {
+            SongPlayerService.SONGS_IN_PHONE.get(song_id).trade_secret = new int[42];
+            SharedPreferences pref_file = SongPlayerService.CONTEXT.getSharedPreferences(TRADE_SECRET_PREF_FILE, Context.MODE_PRIVATE);
             for (Integer i = 0; i < 42; i++) {
                 Integer priority = pref_file.getInt(song_id.toString()+" "+i.toString(),DEFAULT_PRIORITY);
                 if (priority == DEFAULT_PRIORITY) {
@@ -74,9 +74,9 @@ public class TradeSecret {
                     pref_file_editor.putInt(song_id.toString()+" "+i.toString(),DEFAULT_PRIORITY);
                     pref_file_editor.commit();
                 }
-                FirstPage.SONGS_IN_PHONE.get(song_id).trade_secret[i] = priority;
+                SongPlayerService.SONGS_IN_PHONE.get(song_id).trade_secret[i] = priority;
             }
         }
-        return FirstPage.SONGS_IN_PHONE.get(song_id).trade_secret[current_time_slot];
+        return SongPlayerService.SONGS_IN_PHONE.get(song_id).trade_secret[current_time_slot];
     }
 }
